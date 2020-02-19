0f814d8a8931650febeca9aacf8349211c731f29
bryan lesht <bryan.lesht@colorado.edu>
2020-02-19 03:23:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,11 +11,16 @@ def map(array)
 end
 
 
-def reduce(array,start=0)
+def reduce(array,start=nil)
   total = 0 
   index = 0 
-  
-
+  while index < array.length do 
+    
+    total += yield(array[index])
+    index += 1 
+  end 
+  if start != nil 
+    total += yield(start)
   end 
   total 
 end 
