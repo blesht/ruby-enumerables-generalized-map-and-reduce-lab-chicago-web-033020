355ad4d532fcd8db38770effaeeec0b9220abf8f
bryan lesht <bryan.lesht@colorado.edu>
2020-02-19 03:22:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,14 +14,18 @@ end
 def reduce(array,start=0)
   total = 0 
   index = 0 
-  while index < array.length do 
-    puts "total is #{total}"
-    puts "array @ idnex is #{array[index]}"
-    total += yield(array[index])
-    index += 1 
-  end 
-  if start != 0 
-    total += yield(start)
+  if start != nil 
+    
+    while index < array.length do 
+      puts "total is #{total}"
+      puts "array @ idnex is #{array[index]}"
+      total += yield(array[index])
+      index += 1 
+    end 
+  
+  else 
+
+  
   end 
   total 
 end 
