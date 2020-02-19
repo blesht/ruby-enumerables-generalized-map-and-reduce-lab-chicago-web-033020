2e2d07356a6b477ab83fda0ef883d9b513c341e1
bryan lesht <bryan.lesht@colorado.edu>
2020-02-19 03:21:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,8 @@ def reduce(array,start=0)
   total = 0 
   index = 0 
   while index < array.length do 
+    puts "total is #{total}"
+    puts "array @ idnex is #{array[index]}"
     total += yield(array[index])
     index += 1 
   end 
